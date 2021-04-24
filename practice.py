ebf3b9e2d54a3f9f88ec10eea3e822ea9ed3bd9b
TerriblyClever <danschnaars@gmail.com>
2021-04-23 20:59:26 -0400

    created some basic movement of the red box sprite; able to control with the keys and animate a basic jump movement

--- practice.py
+++ practice.py
@@ -2,15 +2,20 @@ import pygame
 
 pygame.init()
 
-game_window = pygame.display.set_mode((500, 500))
+game_window_x = 500
+game_window_y = 500
+game_window = pygame.display.set_mode((game_window_x, game_window_y))
 pygame.display.set_caption("Change This Title Later")
 
-x = 50
-y = 50
+x_center = game_window_x/2
+y_center = game_window_x/2
 height = 40
 width = 60
 velocity = 5
-game_delay = 25
+game_delay = 15
+
+is_jump = False
+jump_count = 10
 
 run = True
 
@@ -23,20 +28,34 @@ while run:
     
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT]:
-        x -= velocity
+    if keys[pygame.K_LEFT] and x_center > velocity:
+        x_center -= velocity
 
-    if keys[pygame.K_RIGHT]:
-        x += velocity
+    if keys[pygame.K_RIGHT] and x_center < (game_window_x - width - velocity):
+        x_center  += velocity
 
-    if keys[pygame.K_UP]:
-        y -= velocity
+    if not is_jump:
+        if keys[pygame.K_UP] and y_center > velocity:
+            y_center -= velocity
 
-    if keys[pygame.K_DOWN]:
-        y += velocity
-    
+        if keys[pygame.K_DOWN] and y_center < (game_window_y - height - velocity):
+            y_center  += velocity
+
+        if keys[pygame.K_SPACE]:
+            is_jump = True
+    else:
+        if jump_count >= -10:
+            neg = 1
+            if jump_count < 0:
+                neg = -1
+            y_center -= (jump_count ** 2) * 0.5 * neg #multiplier to slow down the jump
+            jump_count -= 1
+        else:
+            is_jump = False
+            jump_count = 10
+        
     game_window.fill((0,0,0))
-    pygame.draw.rect(game_window, (255, 0, 0), (x, y, width, height))
+    pygame.draw.rect(game_window, (255, 0, 0), (x_center, y_center, width, height))
     pygame.display.update()
     
 pygame.quit()
